6072df867c491bf34c525cd573f98e2f76041ba2
Braden Irish <braden.irish@gmail.com>
2017-02-20 21:45:05 -0700

    Add The Actuall Code -_-

---
+++ src/org/usfirst/frc/team5461/robot/OI.java
@@ -0,0 +1,76 @@
+package org.usfirst.frc.team5461.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+import org.usfirst.frc.team5461.robot.commands.DecreasePan;
+import org.usfirst.frc.team5461.robot.commands.DecreaseTilt;
+import org.usfirst.frc.team5461.robot.commands.ExampleCommand;
+import org.usfirst.frc.team5461.robot.commands.IncreasePan;
+import org.usfirst.frc.team5461.robot.commands.IncreaseTilt;
+import org.usfirst.frc.team5461.robot.commands.PanAndTiltCommand;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	public Joystick leftstick = new Joystick(0);
+	public Joystick rightstick = new Joystick(0);
+	public JoystickButton aButton = new JoystickButton(leftstick,0); 
+	public DPadButton dpadUp = new DPadButton(new Point(0, 1), leftstick);
+	public DPadButton dpadRight = new DPadButton(new Point(1, 0), leftstick);
+	public DPadButton dpadDown = new DPadButton(new Point(0, -1), leftstick);
+	public DPadButton dpadLeft = new DPadButton(new Point(-1, 0), leftstick);
+	
+	public OI() {
+		dpadUp.whenPressed(new IncreaseTilt());
+		dpadDown.whenPressed(new DecreaseTilt());
+		dpadLeft.whenPressed(new DecreasePan());
+		dpadRight.whenPressed(new IncreasePan());
+		dpadUp.whenPressed(new PanAndTiltCommand());
+		dpadDown.whenPressed(new PanAndTiltCommand());
+		dpadLeft.whenPressed(new PanAndTiltCommand());
+		dpadRight.whenPressed(new PanAndTiltCommand());
+	}
+	
+	public Point getDPadValue(Joystick joystick) {
+		int direction = joystick.getPOV(0);
+		Point point = new Point();
+		point.y = (int)Math.cos(Math.toRadians(direction));
+		point.x = (int)Math.sin(Math.toRadians(direction));
+		return point;
+	}
+	private class DPadButton extends Button {
+		private Point _point;
+		private Joystick _joystick;
+		public DPadButton(Point point, Joystick joystick) {
+			_point = point;
+			_joystick = joystick;
+		}
+
+		@Override
+		public boolean get() {
+			Point dpadPoint = getDPadValue(_joystick);
+			if (_point.x == dpadPoint.x && _point.y == dpadPoint.y) {
+				return true;
+			}
+			return false;
+		}
+}
+
+private static class Point {
+	public int x;
+	public int y;
+
+	public Point() {
+		/* no op */
+	}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+}
---
+++ src/org/usfirst/frc/team5461/robot/subsystems/PanAndTilt.java
@@ -0,0 +1,98 @@
+package org.usfirst.frc.team5461.robot.subsystems;
+import org.usfirst.frc.team5461.robot.commands.PanAndTiltCommand;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class PanAndTilt extends Subsystem {
+	Servo pan;
+	Servo tilt;
+	double tiltVal = 0.5;
+	double panVal = 0.5;
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+       setDefaultCommand(new PanAndTiltCommand());
+    }
+    public void incTilt() {
+    	tiltVal+=0.1;
+        if (tiltVal <= 0.2) {
+        	tiltVal = 0.2;
+        }
+        if (tiltVal >= 0.8) {
+        	tiltVal = 0.8;
+        }
+        if (panVal <= 0) {
+        	panVal = 0;
+        }
+        if (panVal >= 1.0) {
+        	panVal = 1.0;
+        }
+
+    } 
+    public void decTilt() {
+    	tiltVal-=0.1;
+        if (tiltVal <= 0.2) {
+        	tiltVal = 0.2;
+        }
+        if (tiltVal >= 0.8) {
+        	tiltVal = 0.8;
+        }
+        if (panVal <= 0) {
+        	panVal = 0;
+        }
+        if (panVal >= 1.0) {
+        	panVal = 1.0;
+        }
+
+    }
+    public void incPan() {
+    	panVal+=0.1;
+        if (tiltVal <= 0.2) {
+        	tiltVal = 0.2;
+        }
+        if (tiltVal >= 0.8) {
+        	tiltVal = 0.8;
+        }
+        if (panVal <= 0) {
+        	panVal = 0;
+        }
+        if (panVal >= 1.0) {
+        	panVal = 1.0;
+        }
+
+    }
+    public void decPan() {
+    	panVal-=0.1;
+        if (tiltVal <= 0.2) {
+        	tiltVal = 0.2;
+        }
+        if (tiltVal >= 0.8) {
+        	tiltVal = 0.8;
+        }
+        if (panVal <= 0) {
+        	panVal = 0;
+        }
+        if (panVal >= 1.0) {
+        	panVal = 1.0;
+        }
+    }
+    public PanAndTilt(){
+    	pan = new Servo(0);
+    	tilt = new Servo(1);
+    }
+
+    public void move(){
+    	pan.set(panVal);
+    	tilt.set(tiltVal);
+    	System.out.println("panVal: " + panVal);
+    	System.out.println("tiltVal: " + tiltVal);
+    	System.out.println("PanPos: " + pan.getPosition());
+    	System.out.println("TiltPos: " + tilt.getPosition());
+    }
+
+}
+
